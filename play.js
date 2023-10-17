12968a5a742949440805a2e88089dcaf8f326cd5
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-17 14:21:37 -0700

    Connected to snek-multiplayer server. Created idle / server disconnect message using events

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+const connect = function() {
+
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("data", () => {
+    console.log("🐍 hsssssssssss 🐍\n🔥💀 u r dead 💀🔥\n u idled 4 2 long\n🐍 hsssssssssss 🐍");
+  });
+
+  return conn
+}
+
+
+
+console.log("Connecting ...");
+connect();
