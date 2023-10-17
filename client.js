27572b3efeead6937c176d303a427c71f93fb6c8
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-17 16:40:43 -0700

    Add setTimeout and setInterval functions to move snake around. For experimenting purposes only

--- client.js
+++ client.js
@@ -1,6 +1,18 @@
 const net = require("net");
 const connect = function() {
 
+  const welcomeMessage = `
+  🔥💀 WELCOME TO: 💀🔥
+   ⚔️🐍🔥💥 SNEK 💥🔥🐍⚔️
+    🐍 hisssssssss 🐍
+`;
+  const idleMessage = `
+         🐍
+🔥💀 U HAVE DIED 💀🔥
+  too many idleing
+  🐍 hisssssssss 🐍
+`;
+
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
@@ -9,27 +21,34 @@ const connect = function() {
   conn.setEncoding("utf8");
 
   conn.on("data", () => {
-    console.log(
-      `
-              🐍
-      🔥💀 U HAVE DIED 💀🔥
-        too many idleing
-        🐍 hissssssss 🐍
-      `
-    )
+    console.log(idleMessage)
   });
 
   conn.on("connect", () => {
-    console.log(
-      `
-      Congratulations Player!
-       You have successfully
-       entered the kigndom of
-          ⚔️🐍🔥 SNEK 🔥🐍⚔️`
-    )
-    conn.write("Name:  Neo")
-  })
 
+    console.log(welcomeMessage);
+
+    conn.write("Name:  Neo");
+
+  });
+
+  /* Experiment moving our snake with 'hard-coding' using setTimeout and setInterval
+
+  conn.on('connect', () => {
+    setTimeout(() => {
+      conn.write('Move: up');
+      setTimeout(() => {
+        conn.write('Move: left');
+      }, 50);
+    }, 50);
+  });
+
+    conn.on('connect', () => {
+    setInterval(() => {
+      conn.write('Move: left')
+    }, 50)
+  })
+*/
   return conn
 }
 
