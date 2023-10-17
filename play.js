b60e11df84ade6cc40a74ae29ad723abf021d640
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-17 14:37:29 -0700

    moved connect function to client.js file to allow for better code/function mamagement

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-const net = require("net");
-
-const connect = function() {
-
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.setEncoding("utf8");
-
-  conn.on("data", () => {
-    console.log("🐍 hsssssssssss 🐍\n🔥💀 u r dead 💀🔥\n u idled 4 2 long\n🐍 hsssssssssss 🐍");
-  });
-
-  return conn
-}
-
-
+// const net = require("net");
+const { connect } = require("./client.js")
 
 console.log("Connecting ...");
 connect();
