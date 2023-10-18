f2c0ce0f2872a371a430dc882c1cbbcbbfaacda3
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-18 16:33:14 -0700

    keep constants organized and accesible to other files

---
+++ constants.js
@@ -0,0 +1,30 @@
+const IP = 'localhost';
+const PORT = 50541;
+
+const welcomeMessage = `
+🔥💀 WELCOME  TO 💀🔥
+ ⚔️🐍🔥💥 SNEK 💥🔥🐍⚔️
+  🐍 hisssssssss 🐍
+`;
+
+const idleMessage = `
+       🐍
+🔥💀 U HAVE DIED 💀🔥
+  too many idleing
+  🐍 hisssssssss 🐍
+`;
+
+const exitMessage = `
+    You have left
+🐍 the snek kingdom 🐍
+  🖖 safe travels 🖖
+`;
+
+
+module.exports = {
+  IP,
+  PORT,
+  welcomeMessage,
+  idleMessage,
+  exitMessage
+};
