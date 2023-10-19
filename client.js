51256dbbeb175cc0c795b33775aa0435b48cedf6
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-19 12:34:23 -0700

    now with easy to follow comments

--- client.js
+++ client.js
@@ -4,10 +4,12 @@ const { IP, PORT, welcomeMessage, idleMessage } = require("./constants");
 const connect = function() {
 
   const conn = net.createConnection({
+    // establish connection between IP and PORT
     host: IP,
     port: PORT
   });
 
+  // establish compatible encoding, UTF8
   conn.setEncoding("utf8");
 
   conn.on("data", () => {
@@ -15,16 +17,18 @@ const connect = function() {
   });
 
   conn.on("connect", () => {
-
+    // print welcomeMessage upon succesful connection
     console.log(welcomeMessage);
-
+    // indicate player namme
     conn.write("Name:  Neo");
 
   });
 
+  // return conn (object) to allow proper usage across files
   return conn;
 };
 
+// export connect function to allow usage across files
 module.exports = {
   connect
 };
