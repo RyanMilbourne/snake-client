8ada2df7a78225bfeb80ca6062083ad1f7e38011
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-19 12:18:41 -0700

    snake will continue to move in direction decided by WASD keystroke

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 // store the active TCP connection object
 const { exitMessage } = require("./constants");
 let connection;
+let intervalID;
 
 const setupInput = function(conn) {
   connection = conn;
@@ -18,16 +19,21 @@ const handleUserInput = function(key) {
     console.log(exitMessage, '\n')
     process.exit();
   }
+  // clear current interval (movement) before starting NEW interval (movement)
+  if (intervalID) {
+    clearInterval(intervalID);
+  }
   // switch key with the following cases
   switch (key) {
     // WASD controls to move up/left/down/right
-    case 'w': connection.write("Move: up");
+    // if setInterval returns true, snake continues movement in direction
+    case 'w': intervalID = setInterval(() => { connection.write("Move: up"); }, 100);
       break;
-    case 'a': connection.write("Move: left");
+    case 'a': intervalID = setInterval(() => { connection.write("Move: left"); }, 100);
       break;
-    case 's': connection.write("Move: down");
+    case 's': intervalID = setInterval(() => { connection.write("Move: down"); }, 100)
       break;
-    case 'd': connection.write("Move: right");
+    case 'd': intervalID = setInterval(() => { connection.write("Move: right"); }, 100);
       break;
     // canned messages
     case 'o': connection.write("Say: pardon me!");
